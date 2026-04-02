92e5ebdcd2e98451c3cf20764a19605e8fe91198
Pavel Getsman <135528929+onqqr@users.noreply.github.com>
2026-04-02 19:27:42 +0300

    update home page (#2)
    * feat: add prettier + eslint rules
    
    * update home page

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,13 +1,14 @@
-import { Component, signal } from '@angular/core';
+import { ChangeDetectionStrategy, Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Home } from './components/home/home';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, Home],
   templateUrl: './app.html',
   styleUrl: './app.scss',
+  changeDetection: ChangeDetectionStrategy.Eager,
 })
 export class App {
-  protected readonly title = signal('localx');
-  public value = 'test';
+  title = 'homes';
 }
